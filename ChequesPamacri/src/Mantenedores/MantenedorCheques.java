5f979397f9eda8bdff9e6747386bd9aefa91c3f2
Francisco <Francisco@DESKTOP-48T0A0M>
2019-12-19 22:31:25 -0300

    busqueda cheque

--- ChequesPamacri/src/Mantenedores/MantenedorCheques.java
+++ ChequesPamacri/src/Mantenedores/MantenedorCheques.java
@@ -19,13 +19,70 @@ import java.util.ArrayList;
  */
 public class MantenedorCheques {
     /*
-    
-        public int ingresar(id)
+
         public void modificiar(id)
         public void eliminar(id)
-        public ArrayList listarCheques()
-        public ArrayList listarChequesSinCobrar(estado)
     */
+    
+    public void ingresar(Cheque cheque) throws Exception{
+        
+        ConexionBD conexion = new ConexionBD();
+        ArrayList<Cheque> arrCheques = new ArrayList<>();
+        
+        Connection conn = conexion.getConnection();
+            String query = "INSERT INTO cheque (`fecha`, `nroCheque`, `monto`, `cobro`, `estado`, `nroFactura`, `Proveedor_idProveedor`) "
+                         + "VALUES (?,?,?,?,?,?,?)";
+            PreparedStatement stmt=conn.prepareStatement(query);
+            
+            stmt.setDate(1,java.sql.Date.valueOf(cheque.getFechaEmision()));
+            stmt.setString(2,cheque.getNroCheque());
+            stmt.setDouble(3,cheque.getMonto());
+            stmt.setDate(4,java.sql.Date.valueOf(cheque.getFechaCobro()));
+            stmt.setString(5,cheque.getEstado());
+            stmt.setString(6,cheque.getNroFactura());
+            stmt.setInt(7,cheque.getIdProveedor());
+            ResultSet rs=stmt.executeQuery();  
+            
+            
+    }
+    
+    public Cheque obtenerChequePorId(int idCheque) throws Exception{
+        
+        ConexionBD conexion = new ConexionBD();
+        
+        try {
+            
+            Connection conn = conexion.getConnection();
+            String query = "select * from cheque where idCheque = ?";
+            PreparedStatement stmt=conn.prepareStatement(query);
+            
+            stmt.setInt(1,idCheque);
+            ResultSet rs=stmt.executeQuery();  
+            Cheque obtenido = new Cheque();
+            if (rs.next()) {
+                obtenido.setId(rs.getInt(1));
+                obtenido.setFechaEmision(rs.getDate(2).toLocalDate());
+                obtenido.setNroCheque(rs.getString(3));
+                obtenido.setMonto(rs.getInt(4));
+                obtenido.setFechaCobro(rs.getDate(5).toLocalDate());
+                obtenido.setEstado(rs.getString(6));
+                obtenido.setNroFactura(rs.getString(7));
+                obtenido.setIdProveedor(rs.getInt(8));
+            }
+            else{
+                throw new Exception("No se ha encontrado nada");
+            }
+                
+            
+            
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+        
+        
+        
+        return null;
+    }
     
     public ArrayList listarChequesSinCobrar() throws Exception{
         

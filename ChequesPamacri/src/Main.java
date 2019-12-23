349361fe48b07f98d74f6b876b1492c6405fafa8
Francisco <Francisco@DESKTOP-48T0A0M>
2019-12-23 20:45:59 -0300

    clase main ( starter )

---
+++ ChequesPamacri/src/Main.java
@@ -0,0 +1,19 @@
+
+import chequespamacri.Login;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Francisco
+ */
+public class Main {
+    public static void main(String[] args) {
+        Login login = new Login();
+        login.setVisible(true);
+    }
+}
